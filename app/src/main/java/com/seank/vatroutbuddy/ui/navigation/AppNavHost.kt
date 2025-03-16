187a6393e5dde93146d00fb003f157c3cea2c78f
Sean <seank199@gmail.com>
2025-03-16 18:27:28 -0400

    Refactor dialog animation for reusability

--- app/src/main/java/com/seank/vatroutbuddy/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/seank/vatroutbuddy/ui/navigation/AppNavHost.kt
@@ -1,27 +1,11 @@
 package com.seank.vatroutbuddy.ui.navigation
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.window.DialogWindowProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.dialog
@@ -30,12 +14,11 @@ import com.seank.vatroutbuddy.AppConfig
 import com.seank.vatroutbuddy.domain.model.StockingInfo
 import com.seank.vatroutbuddy.ui.features.detail.StockingDetailScreen
 import com.seank.vatroutbuddy.ui.features.notifications.NotificationsScreen
-import com.seank.vatroutbuddy.ui.features.stockings.StockingsScreen
 import com.seank.vatroutbuddy.ui.features.settings.AboutScreen
 import com.seank.vatroutbuddy.ui.features.settings.ContributionsScreen
 import com.seank.vatroutbuddy.ui.features.settings.DebugMenuScreen
 import com.seank.vatroutbuddy.ui.features.settings.SettingsScreen
-import kotlinx.coroutines.delay
+import com.seank.vatroutbuddy.ui.features.stockings.StockingsScreen
 
 @Composable
 fun AppNavHost(modifier: Modifier = Modifier) {
@@ -71,61 +54,21 @@ fun AppNavHost(modifier: Modifier = Modifier) {
 
             dialog(
                 route = NavigationRoutes.StockingDetail.route,
-                dialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+                dialogProperties = DialogProperties(
+                    usePlatformDefaultWidth = false
+                ),
             ) {
-                val dialogWindow = (LocalView.current.parent as? DialogWindowProvider)?.window
-
-                LaunchedEffect(Unit) {
-                    dialogWindow?.setWindowAnimations(-1)
-                }
-
                 val stocking = navController.previousBackStackEntry
                     ?.savedStateHandle
                     ?.get<StockingInfo>("stocking")
 
-                var visible by remember { mutableStateOf(false) }
-
-                val cornerRadius: Dp by animateDpAsState(
-                    targetValue = if (visible) 0.dp else 80.dp,
-                    animationSpec = tween(
-                        durationMillis = AppConfig.MODAL_ANIMATION_DURATION * 2,
-                    )
-                )
-
-                LaunchedEffect(Unit) {
-                    visible = true
-                }
-
                 if (stocking != null) {
-                    AnimatedVisibility(
-                        visible,
-                        enter = scaleIn(
-                            animationSpec = tween(AppConfig.MODAL_ANIMATION_DURATION)
-                        ),
-                        exit = scaleOut(
-                            animationSpec = tween(AppConfig.MODAL_ANIMATION_DURATION)
-                        ),
-                    ) {
-                        Surface(
-                            modifier = Modifier.fillMaxSize(),
-                            shape = RoundedCornerShape(cornerRadius)
-                        ) {
-                            StockingDetailScreen(
-                                stocking = stocking,
-                                onBackClick = {
-                                    visible = false
-                                },
-                                modifier = Modifier.fillMaxSize()
-                            )
-                        }
-
-                        // Wait for animation to complete before popping backstack
-                        LaunchedEffect(visible) {
-                            delay(AppConfig.MODAL_ANIMATION_DURATION.toLong())
-                            if (!visible) {
-                                navController.popBackStack()
-                            }
-                        }
+                    TroutBuddyDialog(navController) { onBackClick ->
+                        StockingDetailScreen(
+                            stocking = stocking,
+                            onBackClick = onBackClick,
+                            modifier = Modifier.fillMaxSize()
+                        )
                     }
                 }
             }
@@ -139,22 +82,37 @@ fun AppNavHost(modifier: Modifier = Modifier) {
                 )
             }
 
-            composable(NavigationRoutes.DebugMenu.route) {
-                DebugMenuScreen(
-                    onBackClick = { navController.popBackStack() }
-                )
+            dialog(
+                route = NavigationRoutes.DebugMenu.route,
+                dialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+            ) {
+                TroutBuddyDialog(navController) { onBackClick ->
+                    DebugMenuScreen(
+                        onBackClick = onBackClick
+                    )
+                }
             }
 
-            composable(NavigationRoutes.About.route) {
-                AboutScreen(
-                    onBackClick = { navController.popBackStack() }
-                )
+            dialog(
+                route = NavigationRoutes.About.route,
+                dialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+            ) {
+                TroutBuddyDialog(navController) { onBackClick ->
+                    AboutScreen(
+                        onBackClick = onBackClick
+                    )
+                }
             }
 
-            composable(NavigationRoutes.Contributions.route) {
-                ContributionsScreen(
-                    onBackClick = { navController.popBackStack() }
-                )
+            dialog(
+                route = NavigationRoutes.Contributions.route,
+                dialogProperties = DialogProperties(usePlatformDefaultWidth = false),
+            ) {
+                TroutBuddyDialog(navController) { onBackClick ->
+                    ContributionsScreen(
+                        onBackClick = onBackClick
+                    )
+                }
             }
         }
     }
---
+++ app/src/main/java/com/seank/vatroutbuddy/ui/navigation/TroutBuddyDialog.kt
@@ -0,0 +1,88 @@
+package com.seank.vatroutbuddy.ui.navigation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogWindowProvider
+import androidx.navigation.NavController
+import com.seank.vatroutbuddy.AppConfig
+import kotlinx.coroutines.delay
+
+/**
+ * Dialog wrapper that handles transition animations
+ * and the ability to display over top of the bottom nav
+ */
+@Composable
+fun TroutBuddyDialog(
+    navController: NavController,
+    content: @Composable (onBackClick: () -> Unit) -> Unit
+) {
+    TroutBuddyDialog({ navController.popBackStack() }, content)
+}
+
+@Composable
+fun TroutBuddyDialog(
+    onNavigateBack: () -> Unit,
+    content: @Composable (onBackClick: () -> Unit) -> Unit
+) {
+    val dialogWindow = (LocalView.current.parent as? DialogWindowProvider)?.window
+    LaunchedEffect(Unit) {
+        dialogWindow?.setWindowAnimations(-1)
+    }
+    var visible by remember { mutableStateOf(false) }
+    val cornerRadius: Dp by animateDpAsState(
+        targetValue = if (visible) 0.dp else 80.dp,
+        animationSpec = tween(
+            durationMillis = AppConfig.MODAL_ANIMATION_DURATION * 2,
+        )
+    )
+
+    LaunchedEffect(Unit) {
+        visible = true
+    }
+
+    AnimatedVisibility(
+        visible,
+        enter = scaleIn(
+            animationSpec = tween(AppConfig.MODAL_ANIMATION_DURATION)
+        ),
+        exit = scaleOut(
+            animationSpec = tween(AppConfig.MODAL_ANIMATION_DURATION)
+        ),
+    ) {
+        Surface(
+            modifier = Modifier
+                .background(
+                    MaterialTheme.colorScheme.surface
+                )
+                .fillMaxSize(),
+            shape = RoundedCornerShape(cornerRadius)
+        ) {
+            content({ visible = false })
+        }
+        // Wait for animation to complete before popping backstack
+        LaunchedEffect(visible) {
+            delay(AppConfig.MODAL_ANIMATION_DURATION.toLong())
+            if (!visible) {
+                onNavigateBack()
+            }
+        }
+    }
+}
