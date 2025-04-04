4f05e8bf1c7148756decfb0bf2f6d50c351a23a4
Sean <seank199@gmail.com>
2025-04-04 00:59:03 -0400

    Enable minify and bump to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,15 +15,15 @@ android {
         applicationId = "com.kenkeremath.vatroutspotter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
